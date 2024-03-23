c0278a5f5455e1b4b8d4be55b9aa0f30b7d04744
Jonathan Boada <jonathanboada1@gmail.com>
2024-03-23 12:39:22 -0500

    Ultimos detalles

--- views/paginas/listado.php
+++ views/paginas/listado.php
@@ -2,38 +2,38 @@
 
     <?php foreach ($propiedades as $propiedad) { ?>
 
-        <div class="anuncio">
+    <div class="anuncio">
 
-            <img loading="lazy" src="/imagenes/<?php echo $propiedad->imagen; ?>" alt="anuncio">
+        <img loading="lazy" src="/imagenes/<?php echo $propiedad->imagen; ?>" alt="anuncio">
 
-            <div class="contenido-anuncio">
-                <h3><?php echo $propiedad->titulo; ?></h3>
-                <p><?php echo $propiedad->descripcion; ?></p>
-                <p class="precio">$ <?php echo $propiedad->precio; ?></p>
+        <div class="contenido-anuncio">
+            <h3><?php echo $propiedad->titulo; ?></h3>
+            <p class="descripcion"><?php echo $propiedad->descripcion; ?></p>
+            <p class="precio">$ <?php echo $propiedad->precio; ?></p>
 
-                <ul class="iconos-caracteristicas">
-                    <li>
-                        <img class="icono" loading="lazy" src="/build/img/icono_wc.svg" alt="icono wc">
-                        <p><?php echo $propiedad->wc; ?></p>
-                    </li>
-                    <li>
-                        <img class="icono" loading="lazy" src="/build/img/icono_estacionamiento.svg"
-                            alt="icono estacionamiento">
-                        <p><?php echo $propiedad->estacionamiento; ?></p>
-                    </li>
-                    <li>
-                        <img class="icono" loading="lazy" src="/build/img/icono_dormitorio.svg" alt="icono habitaciones">
-                        <p><?php echo $propiedad->habitaciones; ?></p>
-                    </li>
-                </ul>
+            <ul class="iconos-caracteristicas">
+                <li>
+                    <img class="icono" loading="lazy" src="/build/img/icono_wc.svg" alt="icono wc">
+                    <p><?php echo $propiedad->wc; ?></p>
+                </li>
+                <li>
+                    <img class="icono" loading="lazy" src="/build/img/icono_estacionamiento.svg"
+                        alt="icono estacionamiento">
+                    <p><?php echo $propiedad->estacionamiento; ?></p>
+                </li>
+                <li>
+                    <img class="icono" loading="lazy" src="/build/img/icono_dormitorio.svg" alt="icono habitaciones">
+                    <p><?php echo $propiedad->habitaciones; ?></p>
+                </li>
+            </ul>
 
-                <a href="/propiedad?id=<?php echo $propiedad->id; ?>" class="boton-amarillo-block">
-                    Ver porpiedad
-                </a>
-            </div>
-            <!--.contenido-anuncio -->
+            <a href="/propiedad?id=<?php echo $propiedad->id; ?>" class="boton-amarillo-block">
+                Ver porpiedad
+            </a>
         </div>
-        <!--.anuncio-->
+        <!--.contenido-anuncio -->
+    </div>
+    <!--.anuncio-->
     <?php } ?>
 </div>
 <!--.contenedor-anuncios-->
